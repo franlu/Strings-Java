460db3b362b7191f31ba70311d489a5b4d142e27
Fran Lucena <fran.lucena@gmail.com>
2022-11-30 10:28:40 +0100

    UP Refactoring

--- ejemplo01.java
+++ ejemplo01.java
@@ -1,21 +1,24 @@
-package ejercicio01.string;
-
 import java.util.Scanner;
 
 /**
  *
  * @author javier granizo <javiercelia123@gmail.com>
+ * @author Fran Lucena <fran.lucena@gmail.com>
  */
-public class Ejercicio07String {
+public class Ejemplo01 {
 
    public static void main(String[] args) {
-        System.out.println("dame la frase");
+        
+      System.out.println("Introduce una frase con espacios en blanco al inicio");
             
-            String a=new Scanner(System.in).nextLine();
-            String b=a.trim();
-            int d=a.indexOf(b);
-            String e=a.substring(d);
+      String cadena = new Scanner(System.in).nextLine();
+         
+      // Eliminar espacios en blanco al inicio y al final
+      String cad_aux = cadena.trim();
+        
+      int posicion = cadena.indexOf(cad_aux);
+      String resultado = cadena.substring(posicion);
        
-        System.out.println(e);
+      System.out.println("La frase sin espacios inciales es: " + resultado);
     } 
 }
